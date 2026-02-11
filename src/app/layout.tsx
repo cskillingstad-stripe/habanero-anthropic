6fa56f6f76db2bed0ab2933abbe000f6887fd8b2
CJ Skillingstad <cskillingstad@stripe.com>
2026-02-10 20:19:05 -0700

    suppress ssr warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" {...mantineHtmlProps}>
+    <html lang="en" {...mantineHtmlProps} suppressHydrationWarning>
       <head>
         <ColorSchemeScript />
       </head>
